ebf0f72b3fb9f27cfb0dd1d687bc6cfc6c480954
Charles Randall <charles.randall@gmail.com>
2013-05-05 19:09:15 -0400

    plant

---
+++ gameobjects/plant.lua
@@ -0,0 +1,43 @@
+local Tools = require 'fabricate.tools'
+local PaletteSprite = require 'PaletteSprite'
+local GameObject = require 'gameobject'
+local Player = require 'player'
+
+local Plant = Tools:Class(GameObject)
+
+function Plant:init(world, shape, x, y)
+	GameObject.init(self,world,shape, x,y )
+	self.type = "plant"
+
+	local frames = {
+		"plant1",
+		"plant2",
+		"plant3"
+	}
+	self.sprite = PaletteSprite:new("specialtiles.sprite", frames[math.random(#frames)])
+	
+	return self
+end
+
+function Plant:handleTouch(other)
+	if other:isA(Player) then
+		print('touch plant')
+		other:handleTouch(self)
+	end
+end
+
+function Plant:draw()
+	love.graphics.setColor(0,0,0)
+
+	love.graphics.rectangle("fill", self.x, self.y, 32,32)
+
+	love.graphics.push()
+
+	love.graphics.translate(self.x, self.y)
+	self.sprite:draw()
+
+	love.graphics.pop()
+
+end
+
+return Plant
